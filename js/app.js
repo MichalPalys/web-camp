dbb2f468947270405971d538d635de6e58dac504
Michał Pałys <michal.palys@jsql.pl>
2020-05-28 22:24:03 +0200

    day4

--- js/app.js
+++ js/app.js
@@ -30,47 +30,78 @@
     // });
 
     $(document).ready(function(){
+
+		$('#langs button').on('click', function(){
+			$(this).next().slideToggle();
+		});
+
         $('.main-slider-container').slick({
             infinite: true,
             slidesToShow: 1,
             slidesToScroll: 1,
-            autoplay: true,
-            autoplaySpeed: 2000
+            autoplay: false,
+			autoplaySpeed: 2000,
+			arrows: false,
         });
 
         $('.company-slider-container').slick({
-            infinite: true,
-            slidesToShow: 6,
-            slidesToScroll: 1,
-            prevArrow: '<button type="button" class="slick-prev custom-slick-prev">Previous</button>',
-            nextArrow: '<button type="button" class="slick-next custom-slick-next">Next</button>',
-            responsive: [
-                {
-                  breakpoint: 960,
-                  settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                    infinite: true
-                  }
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          mobileFirst: true,
+          prevArrow: '<button type="button" class="slick-prev custom-slick-prev">Previous</button>',
+          nextArrow: '<button type="button" class="slick-next custom-slick-next">Next</button>',
+          responsive: [
+              {
+                breakpoint: 960,
+                settings: {
+                  slidesToShow: 6,
+                  slidesToScroll: 1,
+                  infinite: true
                 }
-            ]
+              }
+          ]
+        });
+
+        $('.products-container__slider').slick({
+          infinite: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          mobileFirst: true,
+          arrows: false,
+          variableWidth: true,
+          responsive: [
+            {
+              breakpoint: 960,
+                  settings: {
+                    infinite: false,
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    arrows: true,
+                    variableWidth: false,
+                    prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+                    nextArrow: '<button type="button" class="slick-next">Next</button>',
+                  }
+            }
+          ]
         });
 
         $('.insta-container__slider').slick({
-            infinite: true,
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            prevArrow: '<button type="button" class="slick-prev insta-container__slider__custom-slick-prev">Previous</button>',
-            nextArrow: '<button type="button" class="slick-next insta-container__slider__custom-slick-next">Next</button>',
+          infinite: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          mobileFirst: true,
+          prevArrow: '<button type="button" class="slick-prev custom-slick-prev">Previous</button>',
+          nextArrow: '<button type="button" class="slick-next custom-slick-next">Next</button>',
             responsive: [
                 {
                   breakpoint: 960,
                   settings: {
                     infinite: true,
-                    slidesToShow: 1,
+                    slidesToShow: 3,
                     slidesToScroll: 1,
-                    prevArrow: '<button type="button" class="slick-prev custom-slick-prev">Previous</button>',
-                    nextArrow: '<button type="button" class="slick-next custom-slick-next">Next</button>',
+                    prevArrow: '<button type="button" class="slick-prev insta-container__slider__custom-slick-prev">Previous</button>',
+                    nextArrow: '<button type="button" class="slick-next insta-container__slider__custom-slick-next">Next</button>',
                   }
                 }
             ]
