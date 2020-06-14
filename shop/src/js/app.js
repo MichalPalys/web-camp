2f20f304a89f3a698e8d16e6a650812c18de45ec
Michał Pałys <michal.palys@jsql.pl>
2020-06-15 01:00:01 +0200

    day15

--- shop/src/js/app.js
+++ shop/src/js/app.js
@@ -1,6 +1,11 @@
 $('#menu_search input').attr('placeholder','Szukaj....');
 $("#projector_button_observe").prepend("<i class='icon-plus'></i>");
 $("span.benefits__item.--return").html("Darmowe zwroty <b>do 14 dni</b>");
+$(".questions__txt1").html("Masz pytania do tego produktu ?");
+$(".average_opinions_score").html("4.5");
+$(".questions__phone").attr('href', "+48 602 123 123");
+$(".questions__phone").html("+48 602 123 123");
+$(".opinions_add").append("<div class='opinions_add_desc'>Za napisanie opinii ze zdjęciem otrzymasz 5 pkt. w naszym programie lojalnościowym.</div>");
 // $(".benefits__block.--stocks").append("<span>Darmowa dostawa <b>od 300,00 zł</b></span>");
 $("div.benefits__block.--delivery").clone().appendTo("#projector_benefits");
 $("a.product__see ").addClass("btn --large --outline");
@@ -14,6 +19,3 @@ $('#projector_sizes_cont a.select_button').on('click', function(){
         $("#projector_price_maxprice").after("<span class='desc_on'> (-5% Promocja ograniczona )</span>");
     }
 });
-////////
-
-
